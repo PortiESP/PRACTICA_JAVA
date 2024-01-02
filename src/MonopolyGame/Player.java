ad02b2dd42d46cc5f819104eaba8465caa62c458
porti <portiesp@gmail.com>
2024-01-02 23:44:23 +0100

    Player JavaDocs

--- src/MonopolyGame/Player.java
+++ src/MonopolyGame/Player.java
@@ -5,11 +5,22 @@ import java.util.ArrayList;
 
 import src.MonopolyGame.MonopolyCodes.Property;
 
+/**
+ * This class represents a player in the game. It contains the player's name, money, properties and if he is broke. 
+ * 
+ * <p>
+ * The player's name is used to uniquely identify the player, so it is used in the equals method.
+ * </p>
+ * 
+ * <p>
+ * The player also stores the properties he owns, so it is easier to check if he has a monopoly or sell them if he is about to be broke.
+ * </p>
+ */
 public class Player implements Serializable {
   // Attributes
   private String name; // Player name
   private int money; // Player money
-  private boolean isBroken; // Is the player broken?
+  private boolean broke; // Is the player broke?
   private ArrayList<Property> properties = new ArrayList<>(); // Player properties
 
   // Constructors (for serialization)
@@ -20,7 +31,7 @@ public class Player implements Serializable {
   public Player(String name) {
     this.name = name;
     this.money = Const.STARTING_MONEY;
-    this.isBroken = false;
+    this.broke = false;
   }
 
   // Methods
@@ -28,15 +39,27 @@ public class Player implements Serializable {
     return String.format("Player %s [$%d] - %s", this.name, this.money, this.properties.toString());
   }
 
+  /**
+   * Returns a summary of the player's name, money and properties.
+   * 
+   * @return A string of the summary.
+   */
   public String summary() {
     return String.format("%s [$%d]", this.name, this.money);
   }
 
   @Override
+  /**
+   * Checks if the player is the same as another player.
+   * 
+   * @param player The player to compare.
+   * @return True if the players are the same, false otherwise.
+   */
   public boolean equals(Object player) {
     return this.name.equals(((Player) player).name);
   }
 
+  // ---------------------------------------- Getters and Setters ----------------------------------------
   public String getName() {
     return name;
   }
@@ -45,8 +68,8 @@ public class Player implements Serializable {
     return money;
   }
 
-  public boolean isBroken() {
-    return isBroken;
+  public boolean isBroke() {
+    return broke;
   }
 
   public void setName(String name) {
@@ -57,8 +80,8 @@ public class Player implements Serializable {
     this.money = money;
   }
 
-  public void setBroken(boolean isBroken) {
-    this.isBroken = isBroken;
+  public void setBroke(boolean isBroke) {
+    this.broke = isBroke;
   }
 
   public ArrayList<Property> getProperties() {
